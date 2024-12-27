3815f1d461460e1306a0c07267a45fcdface5c66
Prince Suhagiya <princesuhagiya94@gmail.com>
2024-12-27 15:32:46 +0530

    WiP

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require("express");
-const accessToken = "";
 const app = express();
-const path = require("path");
 app.use(express.json());
+const path = require("path");
+const hubspotToken = process.env.HUBSPOT_APP_KEY;
+const corsMiddleware = require("./middleware/corsMiddleware");
 
-const corsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-};
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on http://localhost:${process.env.PORT}`)
+);
 app.use(corsMiddleware);
 
-http: app.get("/proxy/contacts", (req, res) => {
+app.get("/proxy/contacts", (req, res) => {
   res.sendFile(path.join(__dirname, "contact.html"));
 });
 
@@ -26,21 +21,15 @@ app.post("/proxy/contacts", async (req, res) => {
       {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${accessToken}`,
+          Authorization: `Bearer ${hubspotToken}`,
           "Content-Type": "application/json",
         },
         body: JSON.stringify(req.body),
       }
     );
-
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-
-const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
